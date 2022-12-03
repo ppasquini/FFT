65dea545d0c22db7c45e224e46bfed28329dece0
Ciuchino <giorgio.miani@mail.polimi.it>
2022-12-03 16:41:27 +0100

    Add comments to the code

--- FFT_tools.hpp
+++ FFT_tools.hpp
@@ -19,18 +19,21 @@ namespace FFT
         y.resize(N);
 
         if (N==1){
+            //return the first element
             return {*p_x};
         }
 
         cVector even;
         cVector odd;
 
+        //Save the even elements on the first half of y
         even = recursive_fft((p_x), N/2, 2*s);
         for(size_t i=0; i<N/2; ++i){
             y[i] = even[i];
         }
-        odd = recursive_fft((p_x + s), N/2, 2*s);
 
+        //Save the first element on the second half of y
+        odd = recursive_fft((p_x + s), N/2, 2*s);
         for(size_t i=0; i<N/2; ++i){
             y[i + N/2] = odd[i];
         }
@@ -46,18 +49,21 @@ namespace FFT
             Complex p = y[k];
             y[k] = p + o;
             y[k + N/2] = p - o;
-
         }
 
         return y;
     }   
 
     unsigned int bit_reversal( unsigned int value, unsigned int dim){
+        // Number reversed
         unsigned int reversed = 0;
 
         for(size_t i=0; i<dim;i++){
+            //Shift the number to the left
             reversed <<= 1;
+            //If value as a 1 for the first bit from the right, write 1 on the first bit of reversed
             if((value & 1) == 1) reversed ^= 1;
+            //Shift the value of 1 bit to the right
             value >>= 1;
         }
 
@@ -68,6 +74,7 @@ namespace FFT
         cVector y;
         y.resize(x.size());
         unsigned int j = 0;
+        //Save every elements on its reversed position
         for( unsigned int i=0; i<dim; ++i){
             j = bit_reversal(i,std::log2(dim));
             y[j] = x[i];
